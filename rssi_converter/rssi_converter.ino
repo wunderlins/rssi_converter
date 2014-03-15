1630956003ab2470bb650394cde8968dfd48feb3
wus <swunderlin@gmail.com>
2014-03-15 19:29:28 +0100

    reversed values

--- rssi_converter/rssi_converter.ino
+++ rssi_converter/rssi_converter.ino
@@ -2,7 +2,7 @@
  * Convert a PWM signal to analog
  *
  * Expecting a single channel value > 1000ns and < 2000ns. The result shall be
- * min: 5V, max 0V. RSSI is expected that way by APM on an analog 5V pin.
+ * min: 0V, max 5V. RSSI is expected that way by APM on an analog 5V pin.
  *
  * This code is written for a 5V Arduino Pro Mini.
  *
@@ -13,9 +13,8 @@
 #define PPM_MAX 2000
 #define PPM_MIN 1000
 #define ANALOG_OUT_PIN 3
+#define LED_PIN 13
 #define PPM_INT 0 // pin 2, ppm in
-
-//int Chan1Interrupt = 0; // pin 2, ppm in
 
 volatile unsigned long startPulse = 0;
 volatile double val, val_last = 0;
@@ -24,6 +23,7 @@ uint8_t a_out = 0;
 void setup() {
 	// out analog output, 5-0 V
 	pinMode(ANALOG_OUT_PIN, OUTPUT);
+	//pinMode(LED_PIN, OUTPUT);
 	// attaching interrupt to int 0 (pin 2 on arduino pro mini)
 	attachInterrupt(PPM_INT, begin, RISING);
 #ifdef DEBUG
@@ -33,10 +33,11 @@ void setup() {
 
 void loop() {
 	// use last good ppm frame and convert it to an analog value, 0-255
-	// 255 == 5V, 5V = 0%, 0V = 100% on APM:Plane
-	a_out = map(val, PPM_MAX, PPM_MIN, 0, 255);
+	// 255 == 5V, 5V = 100%, 0V = 0% on APM:Plane
+	a_out = map(val, PPM_MAX, PPM_MIN, 255, 0);
 	// write it out to analog pin
 	analogWrite(ANALOG_OUT_PIN, a_out);
+	//analogWrite(LED_PIN, a_out);
 #ifdef DEBUG
 	Serial.print(a_out);
 	Serial.print("\t");
